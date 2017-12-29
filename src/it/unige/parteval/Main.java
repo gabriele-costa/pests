ed2c25ca25f3f388dcc5e04e55118ffe3132eae7
gabriele <yugo.amaril@gmail.com>
2017-12-29 10:49:45 +0100

    Tool v1.0 ready

--- src/it/unige/parteval/Main.java
+++ src/it/unige/parteval/Main.java
@@ -1,25 +1,19 @@
 package it.unige.parteval;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import it.unige.automata.Automaton;
-import it.unige.automata.State;
-import it.unige.automata.Transition;
 import it.unige.automata.impl.DFAutomatonImpl;
 import it.unige.automata.impl.NFAutomatonImpl;
 import it.unige.automata.impl.StateImpl;
-import it.unige.automata.impl.TransitionImpl;
 import it.unige.automata.util.AutomataTextualInterface;
-import it.unige.lts.LTS;
-import it.unige.mu.*;
-import it.unige.parteval.test.Test;
+import it.unige.automata.util.Printer;
 
 public class Main {
 	
@@ -29,17 +23,23 @@ public class Main {
 			+ "INPUT must be an existing file containing a textual description of a finite state agent\n"
 			+ "\n"
 			+ "OPTIONS are a subset of the following:\n"
-			+ "-o=FILE\t\t\t write output on FILE. Writes on standard output if not specified.\n"
+			+ "-o=FILE\t\t\t write output on FILE. Writes on standard output if not specified and option -f is set to txt.\n"
 			+ "-s=FILE\t\t\t read specification from FILE. Uses FALSE if not specified.\n"
-			+ "-a={v1,...,vN}\t\t use v1,...,vN as synchronization actions. Uses {} if not specified.\n"
+			+ "-a={v1,...,vN}\t\t use v1,...,vN as synchronous actions. Uses {} if not specified.\n"
+			+ "-b={v1,...,vN}\t\t use v1,...,vN as asynchronous actions. Uses {} if not specified.\n"
 			+ "-f=txt|svg|png|pdf\t use the specified output format. Omitting this option is equivalent to -f=txt.\n"
+			+ "-v\t\t\t activate verbose output.\n"
 			+ "-h\t\t\t print this message and exit.\n"; 
 	
 	private final static String error = "Invalid arguments";
 
 	public static void main(String[] args) {
 		
+		log("Process started.");
+		
 		Options opt = new Options();
+		
+		log("Parsing inputs.");
 		
 		ArgsType val = validate(args, opt);
 		
@@ -63,7 +63,7 @@ public class Main {
 			}
 			DFAutomatonImpl A = AutomataTextualInterface.read(buffer);
 			
-			DFAutomatonImpl P;
+			DFAutomatonImpl P = null;
 			
 			if(opt.specification == null) {
 				StateImpl i = new StateImpl("FF");
@@ -78,12 +78,58 @@ public class Main {
 				P = AutomataTextualInterface.read(buffer);
 			}
 			
-			// Set<String> G = getGamma();
+			Set<String> G = new HashSet<String>();
 			
-				
-			// NFAutomatonImpl Pp = Projection.partialA(P, A, getSigma(), G);
+			if(opt.sync_actions != null)
+				G.addAll(Arrays.asList(opt.sync_actions));
 			
-			// P = Projection.unify(Pp, G);
+			Set<String> S = new HashSet<String>();
+			
+			if(opt.async_actions != null)
+				S.addAll(Arrays.asList(opt.async_actions));
+			
+			log("Starting partial evaluation.");
+			
+			long t0 = System.currentTimeMillis();
+			
+			NFAutomatonImpl Pp = Projection.partialA(P, A, S, G);
+			
+			long t1 = System.currentTimeMillis() - t0;
+			log("Partial evaluation: Time=" + t1 +" ms, States="+P.getStates().size()+", Transitions=" + P.getTransitions().size()); 
+			
+			DFAutomatonImpl P2 = Projection.unify(Pp, G);
+			
+			long t2 = System.currentTimeMillis() - t1;
+			log("Unification: Time=" + t2 +" ms, States="+P2.getStates().size()+", Transitions=" + P2.getTransitions().size()); 
+			
+			if(opt.output_format == null)
+				opt.output_format = "txt";
+			
+			log("Writing output.");
+			
+			String output = "";
+			
+			if(opt.output_format.equals("txt") || opt.output == null) {
+				output = AutomataTextualInterface.write(P2);
+			} else {
+				Printer.type = opt.output_format;
+				output = Printer.printDotAutomaton(P2, "PartEval");
+			}
+			
+			if(opt.output == null) {
+				System.out.println(output);
+			}
+			else {
+				Printer.createDotGraph(output, "PartEval");
+				File tmp = new File(Printer.outdir + "PartEval."+ Printer.type);
+				try {
+					Files.move(tmp.toPath(), (new File(opt.output)).toPath(), StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					System.out.println("Cannot write on file due to: "+e);
+				}
+			}
+			
+			System.out.println("Completed.");
 		}
 		
 	}
@@ -95,8 +141,10 @@ public class Main {
 
 	private enum ArgsType {VALID, INVALID, HELP};
 	
+	private final static int FIRSTARG = 0;
+	
 	private static ArgsType validate(String[] args, Options opt) {
-		if(args.length < 2) {
+		if(args.length < FIRSTARG + 1) {
 			return ArgsType.INVALID;
 		}
 		else {
@@ -105,10 +153,10 @@ public class Main {
 				return ArgsType.HELP;
 			}
 			else {
-				opt.input = args[1];
+				opt.input = args[FIRSTARG];
 			}
 			
-			for(int i = 2; i < args.length; i++) {
+			for(int i = FIRSTARG + 1; i < args.length; i++) {
 				if(args[i].startsWith("-o")) {
 					if(opt.output != null)
 						return ArgsType.INVALID;
@@ -121,11 +169,23 @@ public class Main {
 					
 					opt.specification = args[i].substring(3);
 				}
+				else if(args[i].startsWith("-v")) {
+					if(verbose)
+						return ArgsType.INVALID;
+					
+					verbose = true;
+				}
 				else if(args[i].startsWith("-a")) {
 					if(opt.sync_actions != null)
 						return ArgsType.INVALID;
 					
 					opt.sync_actions = args[i].substring(4, args[i].length()-1).split(",");
+				}
+				else if(args[i].startsWith("-b")) {
+					if(opt.async_actions != null)
+						return ArgsType.INVALID;
+					
+					opt.async_actions = args[i].substring(4, args[i].length()-1).split(",");
 				}
 				else if(args[i].startsWith("-f")) {
 					if(opt.output_format != null)
@@ -142,11 +202,20 @@ public class Main {
 		
 	}
 	
+	private static boolean verbose = false;
+	
+	private static void log(String msg) {
+		if(verbose) {
+			System.out.println(msg);
+		}
+	}
+	
 	private static class Options {
 		String input;
 		String output;
 		String specification;
 		String output_format;
 		String[] sync_actions;
+		String[] async_actions;
 	}
 }
